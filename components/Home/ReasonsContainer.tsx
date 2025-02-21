a2892707696ed146cd4c576a51ecc92393e7d3b0
Praveen Prasad <praveenprasad2510@gmail.com>
2025-02-21 15:43:17 +0530

    Made responsive of first section in the reasons container

--- components/Home/ReasonsContainer.tsx
+++ components/Home/ReasonsContainer.tsx
@@ -7,20 +7,20 @@ function ReasonsContainer() {
     <>
       <div>
         {/* Section 1 Content  */}
-        <div className="flex flex-row sm:flex-col">
-          <div className="w-2/3">
-            <h1 className="text-[58px] font-[700] uppercase leading-[1]">
+        <div className="flex flex-col gap-8 sm:flex-row sm:gap-0">
+          <div className="w-full  sm:w-2/3">
+            <h1 className="text-[28px] font-[700] uppercase leading-[1] sm:text-[58px]">
               We provide Intensive
               <br /> Digital marketing
               <br /> program
             </h1>
-            <p className="text-[25px] font-[500] capitalize">
+            <p className=" text-[13px]  font-[500] capitalize sm:text-[25px]">
               03 months program with 00 live projects
             </p>
           </div>
-          <div className="w-1/3 flex flex-col ">
+          <div className="w-full  sm:w-1/3 flex flex-col">
             <div className="flex flex-col justify-end gap-5 items-end text-justify">
-              <p className="capitalize font-[20px] ">
+              <p className="font-[13px]  capitalize sm:font-[20px] ">
                 we offer a comprehensive Intensive Digital Marketing Program
                 designed to help businesses establish a strong online presence,
                 attract the right audience, and maximize conversions. Our
@@ -31,7 +31,7 @@ function ReasonsContainer() {
 
               <Link
                 href={"#"}
-                className="underline flex gap-2 text-center font-[16px]"
+                className=" font-[13px] underline flex gap-2 text-center sm:font-[16px]"
               >
                 <p className="flex items-center gap-2">
                   Learn more about this
