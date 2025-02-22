39521f958eeda99fddb61f2db6de8379efb420ad
Praveen Prasad <praveenprasad2510@gmail.com>
2025-02-22 14:24:56 +0530

    Fixed Deployment error

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Hero from "../components/Home/hero";
 import ReasonsContainer from "@/components/Home/ReasonsContainer";
-import Contact from "../app/contact/page";
+
 
 function page() {
   return (
