1ed063334dc65c2c9e4a6f8c16af631b3a5662aa
Praveen <praveenprasad2510@gmail.com>
2025-02-26 09:57:49 +0530

    Fixed small errors for deployment

--- components/Home/TheCourse.tsx
+++ components/Home/TheCourse.tsx
@@ -1,7 +1,7 @@
 import { div } from "framer-motion/client";
 import React from "react";
 import Image from "next/image";
-import { Fullscreen } from "lucide-react";
+
 
 function TheCourse() {
   return (
