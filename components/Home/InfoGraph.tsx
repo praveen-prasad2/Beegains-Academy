23e5b4ac0465a17d1f4f90e6500c149a43a9cf2f
Praveen <praveenprasad2510@gmail.com>
2025-03-17 14:10:42 +0530

    Cropped Steps images

--- components/Home/InfoGraph.tsx
+++ components/Home/InfoGraph.tsx
@@ -6,43 +6,56 @@ function InfoGraph() {
   return (
     <div>
       {/* Part 1  */}
-      <div className="flex flex-col justify-around h-screen w-screen">
+      <div className="flex flex-col justify-around h-[1200px] w-screen">
         <div className="flex justify-center">
           <Image
             src={"/bee-academy-info.png"}
             width={484}
-            height={258}
+            height={258}  
             alt="the course animated image"
           />
         </div>
-        <div className="flex gap-10 justify-center">
-          <Image
-            src={"/infograph1.png"}
-            width={345}
-            height={536}
-            alt="the course animated image"
-          />
-
-          <div className="flex flex-col  items-center">
-            <div className="w-[100px] h-[50px] bg-bee-orange rounded-t-full flex justify-center items-center">
-              <h1 className="text-[20px] font-k2d font-semibold text-white">
-                01
-              </h1>
+        <div className="flex gap-2 justify-center">
+          {[
+            {
+              id: "01",
+              image: "/infograph1.svg",
+              text: "Know The Secrets Of Industry Leading Digital Marketers.",
+            },
+            {
+              id: "02",
+              image: "/infograph2.svg",
+              text: "Take The Data Driven Path.",
+            },
+            {
+              id: "03",
+              image: "/infograph3.svg",
+              text: "Creativity distinguishes you from your competition.",
+            },
+            {
+              id: "04",
+              image: "/infograph4.svg",
+              text: "Real-time learning from the industry.",
+            },
+          ].map((info) => (
+            <div key={info.id} className="flex flex-col items-center group">
+              <div className="w-[100px] h-[50px] bg-bee-orange rounded-t-full flex justify-center items-center">
+                <h1 className="text-[20px] font-k2d font-semibold text-white">
+                  {info.id}
+                </h1>
+              </div>
+              <div className="w-[363px] h-[91px] bg-white shadow-2xl z-10"></div>
+              <div className="w-[288px] h-[349px] bg-white rounded-b-[50px] shadow-2xl flex flex-col justify-around items-center p-2 text-center transition-transform transform group-hover:scale-105 duration-300">
+                <Image
+                  src={info.image}
+                  width={150}
+                  height={150}
+                  alt="infographic image"
+                />
+                <p className="text-[20px] font-k2d">{info.text}</p>
+              </div>
             </div>
-            <div className="w-[363px] h-[91px] bg-white shadow-2xl z-10"></div>
-            <div className="w-[288px] h-[349px] bg-white rounded-b-[50px] shadow-2xl">
-              <Image
-                src={"/infograph1.svg"}
-                width={150}
-                height={150}
-                alt="the course animated image"
-              />
-
-              <p className="text-[20px] font-k2d">
-                Know The Secrets Of Industry Leading Digital Marketers.
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       {/* Part 2  */}
@@ -52,12 +65,14 @@ function InfoGraph() {
           EXCITEMENT
           <br /> BEGINS
         </h1>
-        <Image
-          src={"/steps.png"}
-          width={100}
-          height={728}
-          alt="the course animated image"
-        />
+        <div className="">
+          <Image
+            src={"/steps.png"}
+            width={929}
+            height={820}
+            alt="the course animated image"
+          />
+        </div>
       </div>
     </div>
   );
