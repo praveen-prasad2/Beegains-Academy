3c03b3ea95137b4169de95118f14e63553092bfe
Praveen <praveenprasad2510@gmail.com>
2025-03-25 15:12:17 +0530

    Made the course  component responsive

--- components/Home/TheCourse.tsx
+++ components/Home/TheCourse.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 
 function TheCourse() {
   return (
-    <div className="w-screen h-auto flex flex-col justify-end">
-      <div className="pl-[110px] pr-[110px] pt-[120px]">
-        <h1 className="text-[100px] font-k2d font-[800] text-bee-orange">The Course</h1>
-        <p className="font-k2d font-normal text-[17px]">
+    <div className="w-screen h-auto flex flex-col justify-end gap-10">
+      <div className="pl-[20px] pr-[20px] pt-[50px] sm:pl-[110px] sm:pr-[110px] sm:pt-[120px]">
+        <h1 className="text-[50px] font-k2d font-[800] text-bee-orange sm:text-[100px]">
+          The Course
+        </h1>
+        <p className="text-[13px] font-k2d font-normal sm:text-[17px] text-justify">
           This comprehensive digital marketing course aims to empower
           individuals with the knowledge and skills necessary to thrive in the
           dynamic digital landscape. By providing a learning experience that
@@ -22,10 +24,10 @@ function TheCourse() {
       </div>
       <div>
         <Image
-          src={"/the-course-animated.gif"} 
+          src={"/the-course-animated.gif"}
           width={2000}
           height={446}
-          alt="the course animated image" 
+          alt="the course animated image"
         />
       </div>
     </div>
