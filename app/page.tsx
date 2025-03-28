63cbb2ef0492aa94daea165e1fb74efb61fb3abb
Praveen <praveenprasad2510@gmail.com>
2025-03-28 14:59:16 +0530

    Updated Line Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Hero from "../components/Home/hero";
-// import ReasonsContainer from "@/components/Home/ReasonsContainer";
+import ReasonsContainer from "@/components/Home/ReasonsContainer";
 import TheVision from "@/components/Home/TheVision";
 import TheCourse from "@/components/Home/TheCourse";
 import Design from "@/components/Home/Design";
@@ -18,7 +18,7 @@ function page() {
   return (
     <div>
       <Hero />
-      {/* <ReasonsContainer /> */}
+      <ReasonsContainer />
       <TheVision />
       <InfoGraph />
       <IntensiveDm />
